6705dbd7a3f03138fe0e6e9a25b2ecf9f0271145
Robert Barnett <therobertbarnett@gmail.com>
2018-09-18 14:17:03 -0700

    Cleaned up server/app.js code

--- server/app.js
+++ server/app.js
@@ -14,16 +14,6 @@ app.get('/api/:search', (req, res) => {
     Axios.get(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${req.params.search}&api_key=c4f12db3382e08f679bedf5c5e91b891&format=json`)
     .then(response => info.lastfm = response.data);
     let ebayUrl = `https://svcs.ebay.com/services/search/FindingService/v1`;
-    // ebayUrl += `?OPERATION-NAME=findItemsByKeywords`;
-    // ebayUrl += `&SERVICE-VERSION=1.0.0`;
-    // ebayUrl += `&SECURITY-APPNAME=RobertBa-ArtistMe-PRD-593587284-6ac41d2e`;
-    // ebayUrl += `&GLOBAL-ID=EBAY-US`;
-    // ebayUrl += `&RESPONSE-DATA-FORMAT=JSON`;
-    // ebayUrl += `&callback=_cb_findItemsByKeywords`;
-    // ebayUrl += `&REST-PAYLOAD`;
-    // ebayUrl += `&paginationInput.entriesPerPage=6`;
-    // ebayUrl += `&keywords=${req.params.search}`;
-
     ebayUrl += `?OPERATION-NAME=findItemsAdvanced`;
     ebayUrl += `&SERVICE-VERSION=1.0.0`;
     ebayUrl += `&SECURITY-APPNAME=RobertBa-ArtistMe-PRD-593587284-6ac41d2e`;
